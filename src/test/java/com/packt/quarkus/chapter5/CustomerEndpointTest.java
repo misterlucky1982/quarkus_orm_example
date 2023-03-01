553f10b790286860d555c12c8128ab8538281d47
misterlucky1982 <damonhill1996@tut.by>
2023-03-01 15:03:11 +0100

    test commit

--- src/test/java/com/packt/quarkus/chapter5/CustomerEndpointTest.java
+++ src/test/java/com/packt/quarkus/chapter5/CustomerEndpointTest.java
@@ -3,10 +3,8 @@ package com.packt.quarkus.chapter5;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import javax.json.Json;
 import javax.json.JsonObject;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
